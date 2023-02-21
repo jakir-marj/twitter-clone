228b4dd7b977b1de8874d0c2f59775677c6b54ed
Jakir <jakirahammedraju@gmail.com>
2023-02-22 01:32:30 +0600

    asset add

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:appwrite_test/theme/pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Pallete.backgroundColor,
+      elevation: 0,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Pallete.blueColor,
+    ),
+  );
+}
