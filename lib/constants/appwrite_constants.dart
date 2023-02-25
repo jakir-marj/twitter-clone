64b6270ddfc336b0e48b553112b722e26fb91efb
Jakir <56072645+jakirahamed@users.noreply.github.com>
2023-02-25 12:07:50 +0600

    Desktop Appwrite add

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,31 +1,18 @@
 class AppwriteConstants {
-  static const String databaseId = '63f50d0598803f345271';
-  static const String projectId = '63f50c6cc6cdf722e358';
-  static const String endPoint = 'http://192.168.31.20/v1';
-  static const String usersCollection = '63f9139dab88fb97485f';
+  static const String databaseId = '63f6faa215a1d7cd17ff';
+  static const String projectId = '63f4a5045dc64df90a7c';
+  static const String endPoint = 'http://192.168.68.149/v1';
+  static const String usersCollection = '63f9a20edd0d0a5f5c6c';
 }
 
-//leptop
-//projectID: 63f50c6cc6cdf722e358
-// databaseId = '63f6faa215a1d7cd17ff';
-// Link speed (Receive/Transmit):	100/100 (Mbps)
-// Link-local IPv6 address:	fe80::e3f:8f57:af26:bf84%8
-// IPv4 address:	192.168.31.20
-// IPv4 DNS servers:	192.168.31.1 (Unencrypted)
-// Manufacturer:	Realtek
-// Description:	Realtek PCIe GbE Family Controller
-// Driver version:	1.0.0.14
-// Physical address (MAC):	80-CE-62-4B-D9-D9
+//* leptop
+//! projectID: 63f50c6cc6cdf722e358
+//! databaseId = '63f6faa215a1d7cd17ff';
+//! IPv4 address:	192.168.31.20
+//! usersCollection = '63f9139dab88fb97485f';
 
-//Desktop
-// projectID : 63f4a5045dc64df90a7c
-// databaseId = '63f6faa215a1d7cd17ff';
-// Link speed (Receive/Transmit):	100/100 (Mbps)
-// Link-local IPv6 address:	fe80::9371:bc2:256c:a9c2%5
-// IPv4 address:	192.168.68.149
-// IPv4 DNS servers:	8.8.8.8 (Unencrypted)
-// 1.1.1.1 (Unencrypted)
-// Manufacturer:	Intel Corporation
-// Description:	Intel(R) Ethernet Connection (2) I219-V
-// Driver version:	12.19.1.32
-// Physical address (MAC):	70-4D-7B-2D-0E-8A
+//* Desktop
+//! projectID : 63f4a5045dc64df90a7c
+//! databaseId = '63f6faa215a1d7cd17ff';
+//! IPv4 address:	192.168.68.149
+//! usersCollection = '63f9a20edd0d0a5f5c6c'
